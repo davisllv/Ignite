15d57814e2aa1b5260b34ae59287051b38a1ad2f
davisllv <davisllvsnt57@gmail.com>
2022-02-15 19:04:28 -0300

    responsiveness in header

--- src/Components/Header/Profile.tsx
+++ src/Components/Header/Profile.tsx
@@ -1,15 +1,20 @@
 import { Flex, Box, Text, Avatar } from "@chakra-ui/react";
 
-export function Profile() {
+interface IProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: IProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Davi da Silva dos Santos</Text>
-        <Text color="gray.300" fontSize="small">
-          davisllvsnt57@gmail.com
-        </Text>
-      </Box>
-
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Davi da Silva dos Santos</Text>
+          <Text color="gray.300" fontSize="small">
+            davisllvsnt57@gmail.com
+          </Text>
+        </Box>
+      )}
       <Avatar size="md" name="Davi da Silva dos Santos" />
     </Flex>
   );
--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,9 +1,15 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, useBreakpointValue } from "@chakra-ui/react";
 import { Logo } from "./Logo";
 import { NotificationNav } from "./NotificationNav";
 import { Profile } from "./Profile";
 import { SearchBox } from "./SearchBox";
+
 export function Header() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
   return (
     <Flex
       w="100%"
@@ -16,11 +22,11 @@ export function Header() {
     >
       <Logo />
 
-      <SearchBox />
+      {isWideVersion && <SearchBox />}
 
       <Flex align="center" ml="auto">
         <NotificationNav />
-        <Profile />
+        <Profile showProfileData={isWideVersion} />
       </Flex>
     </Flex>
   );
