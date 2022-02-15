ef298241b9f7c755d6a7e48b4a3af762a14fdd2d
davisllv <davisllvsnt57@gmail.com>
2022-02-15 19:40:58 -0300

    fixing the dashboard responsiveness

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -72,7 +72,7 @@ export default function Dashboard() {
           minChildWidth="320px"
           alignItems="flex-start"
         >
-          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+          <Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
               Inscritos da Semana
             </Text>
@@ -85,7 +85,7 @@ export default function Dashboard() {
               />
             )}
           </Box>
-          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+          <Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
               Taxa de Abertura
             </Text>
