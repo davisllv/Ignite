3d97a56a895d8e6963c4aefa70ad13163c89fd89
davisllv <davisllvsnt57@gmail.com>
2022-02-15 17:57:06 -0300

    create the user page

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,87 @@
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import { Header } from "../../Components/Header";
+import { Sidebar } from "../../Components/Siderbar";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
+
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justifyContent={"space-between"} align="center">
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+            </Heading>
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+            >
+              Criar Novo
+            </Button>
+          </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" width="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuários</Th>
+                <Th>Data de Cadastro</Th>
+                <Th width="2"></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr px="6">
+                <Td>
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Davi da Silva dos Santos</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      davisllvsnt57@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>15 de Fevereiro, 2022</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Criar Novo
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
