d3b2fa3f956fff6f0c2581d9dff971fd0044ba0e
davisllv <davisllvsnt57@gmail.com>
2022-02-16 18:49:43 -0300

    seting the fiction data

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -19,11 +19,18 @@ import { Sidebar } from "../../Components/Sidebar";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Pagination } from "../../Components/Pagination";
 import Link from "next/link";
+import { useEffect } from "react";
 
 export default function UserList() {
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
+  });
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/users")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
   });
 
   return (
