cf4f50e39bd248c9d1f5a895e43bb280b53c3173
davisllv <davisllvsnt57@gmail.com>
2022-02-17 06:31:26 -0300

    creating the pagination

--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -10,7 +10,7 @@ type User = {
 
 export async function getUsers(): Promise<User[]> {
     const { data } = await api.get("users");
-    console.log(data);
+
 
     const users = data.users.map((user) => {
         return {
